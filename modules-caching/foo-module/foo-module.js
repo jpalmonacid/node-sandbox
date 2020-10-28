126c82e31fa45fbf49ebe233ad9066d66abf32f2
Juan Pablo Almonacid <jalmonacid@artear.com>
2020-10-28 16:14:07 -0300

    Try module caching

---
+++ modules-caching/foo-module/foo-module.js
@@ -0,0 +1,1 @@
+const innerModule = require('./inner-module');
---
+++ modules-caching/index.js
@@ -0,0 +1,5 @@
+const fooModule = require('./foo-module/foo-module');
+const innerModule = require('./foo-module/inner-module');
+
+console.log(require);
+
